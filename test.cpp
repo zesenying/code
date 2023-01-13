0a4387d3c7ae611bcbfb2d6a50817e27483260b8
zesenying <zesenying@berkeley.edu>
2023-01-12 20:06:36 -0500

    testing

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello World!"<<endl;
+    string stock = "aapl";
+    string stocks[3] = {"AAPL","MSFT","TSLA"} ;
+    for(int i = 0; 
+        i<3;
+        i++){
+        cout<<stocks[i]<<endl;
+
+
+        }
+
+
+    return 0;
+
+}
+
+
